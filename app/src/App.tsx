e7f57fcd87d04bbf1f48686a456c6aef99857689
stan-dot <stanislawmalinowski09@gmail.com>
2022-11-27 13:04:54 +0000

    added regex

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,44 +1,45 @@
-import './App.css';
-import { atom, PrimitiveAtom, useAtom, useSetAtom } from "jotai";
+import { atom, useAtom } from "jotai";
 import { useEffect } from "react";
+import "./App.css";
+import { BookmarksDisplay } from "./BookmarksDisplay";
 import { getMatchingBookmarks, getTabUrl, getUserFromUrl } from "./utils";
-import { BookmarksDisplay } from './BookmarksDisplay';
 
 const DEFAULT_URL = "https://github.com/";
 const tabUrlAtom = atom(DEFAULT_URL);
 
-const DEFAULT_BOOKMARKS: chrome.bookmarks.BookmarkTreeNode[] = []
-const bkmrksAtom = atom(DEFAULT_BOOKMARKS)
+const DEFAULT_BOOKMARKS: chrome.bookmarks.BookmarkTreeNode[] = [];
+const bkmrksAtom = atom(DEFAULT_BOOKMARKS);
 
-const userFromUrl = atom((get) => getUserFromUrl(get(tabUrlAtom)))
-
+const userFromUrl = atom((get) => getUserFromUrl(get(tabUrlAtom)));
 
 export default function App(): JSX.Element {
   const [url, setUrl] = useAtom(tabUrlAtom);
   const [user] = useAtom(userFromUrl);
   const [bkmrksList, setBkmrksList] = useAtom(bkmrksAtom);
 
+  const bookmarkCallback = (list: chrome.bookmarks.BookmarkTreeNode[]): void =>
+    setBkmrksList(list);
   useEffect(() => {
     getTabUrl().then((url: string) => {
       setUrl(url);
       const user = getUserFromUrl(url);
-      getMatchingBookmarks(user).then(list => {
-        setBkmrksList(list);
-      })
+      getMatchingBookmarks(user, bookmarkCallback);
       console.log(url, user, bkmrksList);
-    })
-    return () => { }
-  }, [])
+    });
+    return () => { };
+  }, []);
 
   console.log(url, user, bkmrksList);
 
   return (
     <div className="App">
       <header className="App-header">
-        <p>url:{url} </p>
+        <p>url:{url}</p>
         <p>user:{user}</p>
         <BookmarksDisplay list={bkmrksList} />
-        <button onClick={() => getMatchingBookmarks(user)}>search</button>
+        <button onClick={() => getMatchingBookmarks(user, bookmarkCallback)}>
+          search
+        </button>
       </header>
     </div>
   );
--- app/src/utils.ts
+++ app/src/utils.ts
@@ -1,21 +1,37 @@
-
-// todo use the chrome api
-export async function getMatchingBookmarks(user: string): Promise<chrome.bookmarks.BookmarkTreeNode[]>{
-    chrome.bookmarks.search(user,
-      (s) => console.log(`search finished: ${s}`)
-    );
-  return []
-};
-
-// todo need to use regex
-export function getUserFromUrl(url: string): string {
-  return url
+export async function getMatchingBookmarks(
+  user: string,
+  callback: (list: chrome.bookmarks.BookmarkTreeNode[]) => void,
+): Promise<void> {
+  chrome.bookmarks.search(user, (s) => {
+    console.log(`search finished: ${s}`)
+    callback(s);
+  });
 }
 
-export async function getTabUrl(): Promise<string>{
-  const currentTab:chrome.tabs.Tab | undefined = await chrome.tabs.getCurrent();
-  if (!currentTab) {
-    return ""
+export function getUserFromUrl(url: string): string {
+  const regex1 = `^https?://(www\.)?twitter\.com/(#!/)?([^/]+)(/\w+)*$`;
+  const regex2 =
+    `^https?:\/\/(?:www\.)?twitter\.com\/(?:#!\/)?@?([^/?#]*)(?:[?#].*)?$`;
+  const match = url.match(regex1);
+  const match2 = url.match(regex2);
+  try {
+    const v1 = match![1] ?? 'nothing';
+    console.log(v1)
+    const v2 = match2![3] ?? 'nothing';
+    console.log(v2)
+    return v1.concat(v2)
+  } catch (error) {
+    
+    console.log('e', error)
   }
-  return currentTab.url ?? "" 
+  return 'not a twitter url'
+}
+
+export async function getTabUrl(): Promise<string> {
+  const currentTab: chrome.tabs.Tab | undefined = await chrome.tabs
+    .getCurrent();
+  if (!currentTab) {
+    return "";
+  }
+  return currentTab.url ?? "";
 }
