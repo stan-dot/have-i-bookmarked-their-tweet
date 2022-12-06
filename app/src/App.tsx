2c0c26e30e33d931c18d1c665ad8a4fda264c37a
stan-dot <stanislawmalinowski09@gmail.com>
2022-12-06 13:30:59 +0000

    added basic search highlight

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -42,8 +42,8 @@ export default function App(): JSX.Element {
 
   const filteredItems = query === "" ? bkmrksList : bkmrksList.filter(i => {
     return i.title.includes(query.toLowerCase())
+  }).map((i: chrome.bookmarks.BookmarkTreeNode) => { return { ...i, title: getQueryHighlight(i.title, query) } })
 
-  })
   const [searchHighlight, setSearchHighlight] = useState(false);
 
   return (
@@ -68,4 +68,14 @@ export default function App(): JSX.Element {
 }
 
 
-
+// todo highlight of queries
+// https://stackoverflow.com/questions/67765118/how-to-add-bold-style-to-a-substring-in-react
+// an elaborate solution like this one will be needed
+// likewise, capture nested URLs https://regexr.com/39nr7
+function getQueryHighlight(text: string, query: string): string {
+  const reg = new RegExp(query, 'gi');
+  const matches = text.match(reg)
+  console.log(matches);
+  const final_str = text.replace(reg, str => `***${str}***`);
+  return final_str
+}
